ee0503bfb5ea5b37037ed99638e6fcfdf02f22fe
cloudwaddie <25fazedw@cbc.sa.edu.au>
2025-05-12 18:21:41 +0930

    Fix controller function call and update sitemap configuration

--- feature-tracker/src/utils/tasks/controller.js
+++ feature-tracker/src/utils/tasks/controller.js
@@ -2,7 +2,7 @@ import appController from "./apps/controller.js";
 import webController from "./web/controller.js";
 
 export default async function runAllTasks() {
-    //await appController(); // Call the app controller function
+    await appController(); // Call the app controller function
     await webController(); // Call the web controller function
     return "All tasks are running...";
 }
