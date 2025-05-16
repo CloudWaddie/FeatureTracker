3a408dc1a855b1a931aef33046b1ecddc5c2b572
cloudwaddie <cloudwaddie@hackclub.app>
2025-05-16 07:32:40 +0000

    Stuff

--- feature-tracker/server.js
+++ feature-tracker/server.js
@@ -4,7 +4,7 @@ import { parse } from 'url';
 import next from 'next';
 import startScheduler from './src/utils/scheduler.js';
 
-const port = parseInt(process.env.PORT || '3000', 10);
+const port = parseInt(process.env.PORT || '45819', 10);
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
