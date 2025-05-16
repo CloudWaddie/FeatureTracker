655f8485cba2e2e9f8011f924237299a15d32605
CloudWaddie <148834837+CloudWaddie@users.noreply.github.com>
2025-05-16 15:04:33 +0930

    Update route.js

--- feature-tracker/src/app/api/db/setup/route.js
+++ feature-tracker/src/app/api/db/setup/route.js
@@ -57,7 +57,7 @@ export async function GET(request) {
                 }
             }
             );
-            db.run("CREATE TABLE IF NOT EXISTS oldSitemaps (id INTEGER PRIMARY KEY AUTOINCREMENT, siteURL TEXT NOT NULL, url TEXT NOT NULL, lastUpdated TEXT NOT NULL);", (err) => {
+            db.run("CREATE TABLE IF NOT EXISTS oldSitemaps (id INTEGER PRIMARY KEY AUTOINCREMENT, siteURL TEXT NOT NULL, url TEXT NOT NULL, lastUpdated TEXT);", (err) => {
                 if (err) {
                     console.error("Error creating oldSitemaps table:", err.message);
                 } else {
@@ -65,7 +65,7 @@ export async function GET(request) {
                 }
             }
             );
-            db.run("CREATE TABLE IF NOT EXISTS newSitemaps (id INTEGER PRIMARY KEY AUTOINCREMENT, siteURL TEXT NOT NULL, url TEXT NOT NULL, lastUpdated TEXT NOT NULL);", (err) => {
+            db.run("CREATE TABLE IF NOT EXISTS newSitemaps (id INTEGER PRIMARY KEY AUTOINCREMENT, siteURL TEXT NOT NULL, url TEXT NOT NULL, lastUpdated TEXT);", (err) => {
                 if (err) {
                     console.error("Error creating newSitemaps table:", err.message);
                 } else {
