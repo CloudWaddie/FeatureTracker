aab10655f97466a378f72b94806576b658afcb81
cloudwaddie <25fazedw@cbc.sa.edu.au>
2025-05-17 09:50:12 +0930

    Add geminiDates functionality to manage date placeholders and update feed

--- feature-tracker/src/utils/tasks/web/controller.js
+++ feature-tracker/src/utils/tasks/web/controller.js
@@ -1,9 +1,11 @@
 import sitemapController from './sitemaps/controller.js';
 import lmarenaController from './lmarena/controller.js';
+import geminiDatesController from './geminiDates/controller.js';
 
 export default async function webController() {
     console.log("Running web controller...");
     await sitemapController();
     await lmarenaController();
+    await geminiDatesController();
     return "Web controller is running...";
 }
