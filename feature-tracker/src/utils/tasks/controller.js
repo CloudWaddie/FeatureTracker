68800eb1415d60ef791a57d6644d2cdd88a08e29
cloudwaddie <25fazedw@cbc.sa.edu.au>
2025-05-24 14:48:25 +0930

    Enable web controller execution in runAllTasks and add new RSS feed URLs to config

--- feature-tracker/src/utils/tasks/controller.js
+++ feature-tracker/src/utils/tasks/controller.js
@@ -3,6 +3,6 @@ import webController from "./web/controller.js";
 
 export default async function runAllTasks() {
     await appController(); // Call the app controller function
-    //await webController(); // Call the web controller function
+    await webController(); // Call the web controller function
     return "All tasks are running...";
 }
