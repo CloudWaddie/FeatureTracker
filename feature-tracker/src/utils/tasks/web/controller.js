a1827c34356cb506759abe7593fe268559fb87f8
cloudwaddie <25fazedw@cbc.sa.edu.au>
2025-05-26 18:18:50 +0930

    Refactor Android apps API and improve task execution in web controller

--- feature-tracker/src/utils/tasks/web/controller.js
+++ feature-tracker/src/utils/tasks/web/controller.js
@@ -4,12 +4,17 @@ import geminiDatesController from './geminiDates/controller.js';
 import feedController from './feeds/controller.js';
 import domainFinderController from './domainFinders/controller.js';
 
+const controllers = [
+    sitemapController,
+    lmarenaController,
+    geminiDatesController,
+    feedController,
+    domainFinderController
+];
+
 export default async function webController() {
     console.log("Running web controller...");
-    await sitemapController();
-    await lmarenaController();
-    await geminiDatesController();
-    await feedController();
-    await domainFinderController();
+    const results = await Promise.all(controllers.map(controller => controller()));
+    console.log("Web controller tasks completed:", results);
     return "Web controller is running...";
 }
